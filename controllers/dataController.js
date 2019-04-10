3d7f783fd081ea3fe616b76097daf78baa00d44a
jayblack388 <jblackwell072588@gmail.com>
2019-04-10 08:41:38 -0400

    set up basic data model/controller

---
+++ controllers/dataController.js
@@ -0,0 +1,10 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: (req, res) => {
+    db.Data(req.query)
+    .sort({date: -1})
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
+  }
+}
