055f396af342a9666cd8ea856b93c6fbac7833c4
John Blackwell <john.blackwell@rtslabs.com>
2019-04-18 11:24:24 -0400

    fogot to save before commit

--- client/src/components/pages/Auth/Login.js
+++ client/src/components/pages/Auth/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Page, Wrapper } from '../../containers';
 const Login = props => {
+  console.log(props);
   return (
     <Page>
       <Wrapper>I'm the Login Page!</Wrapper>
--- client/src/components/pages/Auth/Signup.js
+++ client/src/components/pages/Auth/Signup.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Page, Wrapper } from '../../containers';
 const Signup = props => {
+  console.log(props);
   return (
     <Page>
       <Wrapper>I'm the Signup Page!</Wrapper>
