9bae8d6f560a7ecf0e7539afedeea9ea0aacb52a
John Blackwell <john.blackwell@rtslabs.com>
2019-04-18 11:21:37 -0400

    superficial homepage

---
+++ client/src/components/pages/Home/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Page, Wrapper } from '../../containers';
+const HomePage = () => {
+  return (
+    <Page>
+      <Wrapper>I'm the HomePage!</Wrapper>
+    </Page>
+  );
+};
+
+export default HomePage;
