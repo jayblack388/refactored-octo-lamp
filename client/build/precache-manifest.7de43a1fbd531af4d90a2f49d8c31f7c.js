8b6ba8b2b542ecab9a61e262a5ec3152bd23ed94
John Blackwell <john.blackwell@rtslabs.com>
2019-04-18 15:19:55 -0400

    remove react-scripts again, clean cache and add /build to repo

---
+++ client/build/precache-manifest.7de43a1fbd531af4d90a2f49d8c31f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "703621afeb3a0b422d6d",
+    "url": "/static/css/main.3dc00393.chunk.css"
+  },
+  {
+    "revision": "703621afeb3a0b422d6d",
+    "url": "/static/js/main.0f96a436.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1de6b1583040f026337a",
+    "url": "/static/css/2.2193043f.chunk.css"
+  },
+  {
+    "revision": "1de6b1583040f026337a",
+    "url": "/static/js/2.2f5fc464.chunk.js"
+  },
+  {
+    "revision": "b67a819c6e4ee2914055f67da23cad47",
+    "url": "/index.html"
+  }
+];
