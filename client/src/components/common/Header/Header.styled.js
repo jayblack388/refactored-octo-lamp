620b9dc502395dd6d620803ad1616c4151326c30
John Blackwell <john.blackwell@rtslabs.com>
2019-04-30 19:23:27 -0400

    semantic elements

--- client/src/components/common/Header/Header.styled.js
+++ client/src/components/common/Header/Header.styled.js
@@ -1,8 +1,14 @@
 import Color from 'color';
 import styled from 'styled-components';
+import DefaultButton from '../Button';
 
-export const Container = styled.div`
+export const Container = styled.nav`
   background-color: #000;
   padding: 0.8rem 1.6rem;
   color: #fff;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const Button = styled(DefaultButton)`
 `;
--- client/src/components/containers/Wrapper/Wrapper.styled.js
+++ client/src/components/containers/Wrapper/Wrapper.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
