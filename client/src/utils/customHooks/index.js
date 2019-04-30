de5d9fdf4e9a7ad2cadda21568c52ec6355e8f95
John Blackwell <john.blackwell@rtslabs.com>
2019-04-29 22:56:45 -0400

    add useLocalStorage

--- client/src/utils/customHooks/index.js
+++ client/src/utils/customHooks/index.js
@@ -1,2 +1,3 @@
 export { default as useForceUpdate } from './useForceUpdate';
+export { default as useLocalStorage } from './useLocalStorage';
 export { default as useRouter } from './useReactRouter';
