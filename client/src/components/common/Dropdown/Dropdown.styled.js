0ee716c7c2c7d98eccf6831813fcd34f4081508e
John Blackwell <john.blackwell@rtslabs.com>
2019-05-09 18:05:53 -0400

    create Dropdown components and add react-spring for animation

---
+++ client/src/components/common/Dropdown/Dropdown.styled.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import { animated } from 'react-spring';
+import DefaultButton from '../Button';
+
+export const DropdownContainer = styled.div`
+  position: relative;
+`;
+
+export const DropdownMenu = styled(animated.ul)`
+  visibility: ${props => (props.open ? 'visible' : 'hidden')};
+  background-color: ${props => props.color || '#fff'};
+  border: 1px solid #ebebeb;
+  position: absolute;
+  padding: 0;
+  border-radius: 0.8rem;
+  margin-top: 1rem;
+  width: 100%;
+  height: fit-content;
+  right: 0;
+  top: 25px;
+`;
+
+export const DropdownItem = styled.li`
+  display: flex;
+  cursor: pointer;
+  color: #000;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  margin: 3px;
+`;
+
+export const DropdownToggler = styled(DefaultButton)`
+  border: 1px solid #ebebeb;
+  transition: none;
+  box-shadow: 0 2px 3px #999;
+  width: 100%;
+`;
