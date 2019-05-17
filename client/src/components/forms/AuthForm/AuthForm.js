6f6d16dfcbc21621a76a13c0ceb27250fa8c8a89
John Blackwell <john.blackwell@rtslabs.com>
2019-05-17 19:06:41 -0400

    need to finish up confirm email/code

--- client/src/components/forms/AuthForm/AuthForm.js
+++ client/src/components/forms/AuthForm/AuthForm.js
@@ -1,31 +1,63 @@
 import React, { useState } from 'react';
-import { Button, Input, InputContainer, Label, LabelText } from '../../common';
+import { Button, Input, LinkButton } from '../../common';
 import { Wrapper } from '../../containers';
 import { useRouter } from '../../../utils/customHooks';
 import { Form } from './AuthForm.styled';
 
 const AuthForm = props => {
-  const { type } = props;
+  const { type, setType } = props;
   const isLogin = type === 'login';
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [code, setCode] = useState('');
   const { history } = useRouter();
 
-  let body = {
+  const body = {
     email,
     password,
+  };
+  const name = {
+    firstName,
+    lastName,
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-    isLogin ? props.onLogin(body) : props.onSignUp();
+    isLogin ? props.onLogin(body) : props.onSignUp({ ...body, name });
     history.push('/');
+  };
+
+  const toggleType = e => {
+    e.preventDefault();
+    setType(isLogin ? 'signup' : 'login');
   };
 
   return (
     <Form isLogin={isLogin} onSubmit={handleSubmit}>
-      <Wrapper height={isLogin ? '80%' : '50%'}>
+      <h3>{isLogin ? 'Login' : 'Sign Up'}</h3>
+      {!isLogin && (
+        <Wrapper height="20%" direction="row">
+          <Input
+            width="80%"
+            inputWidth="70%"
+            label="First Name:"
+            type="text"
+            value={firstName}
+            onChange={e => setFirstName(e.target.value)}
+          />
+          <Input
+            width="80%"
+            inputWidth="70%"
+            label="Last Name:"
+            type="text"
+            value={lastName}
+            onChange={e => setLastName(e.target.value)}
+          />
+        </Wrapper>
+      )}
+      <Wrapper height={isLogin ? '80%' : '40%'}>
         <Input
           width="90%"
           label="Username:"
@@ -41,11 +73,22 @@ const AuthForm = props => {
           onChange={e => setPassword(e.target.value)}
         />
       </Wrapper>
-      <Button
-        primary
-        onClick={handleSubmit}
-        message={isLogin ? 'Login' : 'Sign Up'}
-      />
+      <Wrapper height="20%">
+        <LinkButton
+          blue
+          message={
+            isLogin
+              ? "Don't have an account?\nRegister here."
+              : 'Already have an account?\nLogin here.'
+          }
+          onClick={toggleType}
+        />
+        <Button
+          primary
+          message={isLogin ? 'Login' : 'Sign Up'}
+          onClick={handleSubmit}
+        />
+      </Wrapper>
     </Form>
   );
 };
--- client/src/components/forms/AuthForm/AuthForm.styled.js
+++ client/src/components/forms/AuthForm/AuthForm.styled.js
@@ -3,7 +3,8 @@ import flex from '../../../utils/styles/flex';
 
 export const Form = styled.form`
   ${props => flex(props)}
-  height: ${props => (props.isLogin ? '30rem' : 'fit-content')};
+  justify-content: ${props => (props.isLogin ? 'center' : 'space-between')}
+  height: 30rem;
   width: 30rem;
   box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
   border-radius: 0.4rem;
