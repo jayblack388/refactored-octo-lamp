a1b9de4534396253b6cd26d52a6c8c138ba4dcdc
John Blackwell <john.blackwell@rtslabs.com>
2019-05-17 17:11:37 -0400

    make input components

---
+++ client/src/components/common/Input/Input.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: ${props => props.padding || '1.6rem'}
+  align-items: ${props => props.align || 'center'};
+  justify-content: ${props => props.justify || 'center'};
+  width: ${props => props.width || '40%'};
+`;
+
+export const Input = styled.input.attrs({
+  type: props => props.type || 'text',
+  placeholder: props => props.placeholder || 'Fll me out 🖊',
+})`
+  font-size: 0.8rem;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  width: ${props => props.width || '40%'};
+`;
+
+export const LabelText = styled.span`
+  text-align: center;
+  padding-bottom: 0.8rem;
+`;
--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -2,4 +2,5 @@ export { default as BrandLink } from './BrandLink';
 export { default as Button } from './Button';
 export { default as Dropdown } from './Dropdown';
 export { default as Header } from './Header';
+export { default as Input } from './Input';
 export { default as Loader } from './Loader';
