00a3efb27017a3291910ef892537dba3f3b06493
John Blackwell <john.blackwell@rtslabs.com>
2019-05-24 15:37:29 -0400

    added some more hooks

--- client/src/utils/customHooks/index.js
+++ client/src/utils/customHooks/index.js
@@ -1,4 +1,6 @@
 export { default as useForceUpdate } from './useForceUpdate';
 export { default as useHover } from './useHover';
 export { default as useLocalStorage } from './useLocalStorage';
+export { default as useKeyPress } from './useKeyPress';
+export { default as useOnClickOutside } from './useOnClickOutside';
 export { default as useRouter } from './useReactRouter';
