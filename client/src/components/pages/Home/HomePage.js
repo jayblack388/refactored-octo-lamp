47d38237df780075fb36eb71883aafbcc82146e9
John Blackwell <john.blackwell@rtslabs.com>
2019-05-27 16:06:33 -0400

    toast POC

--- client/src/components/pages/Home/HomePage.js
+++ client/src/components/pages/Home/HomePage.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 import { Page, Wrapper } from '../../containers';
 import { Button, Loader } from '../../common';
 import { useGlobalState } from '../../../store/GlobalState';
 const HomePage = props => {
-  const [{ user: { user, isLoading } }, ] = useGlobalState();
+  const [
+    {
+      user: { user, isLoading },
+    },
+  ] = useGlobalState();
+  const notify = () => toast('Wow so easy !');
 
   return (
     <Page>
       <Wrapper>
-        <Loader isLoading={isLoading} >
+        <Loader isLoading={isLoading}>
           <span>I'm the Home Page</span>
           {user.details.email}
-          <Button message={'stuff'} />
+          <Button onClick={notify} message={'stuff'} />
         </Loader>
       </Wrapper>
     </Page>
