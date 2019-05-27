903baded615f4a41a9e1b8c5a7fad9af5ba1b37e
John Blackwell <john.blackwell@rtslabs.com>
2019-05-27 16:05:47 -0400

    create toast component

--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -4,3 +4,4 @@ export { default as Dropdown } from './Dropdown';
 export { default as Header } from './Header';
 export { default as Input } from './Input';
 export { default as Loader } from './Loader';
+export { default as ToastContainer } from './ToastContainer';
