c682fb1bb313a5c68a9865168899fe2e31764423
John Blackwell <john.blackwell@rtslabs.com>
2019-07-12 19:14:10 -0700

    add isComplete and isFavorite to models

--- models/listItem.js
+++ models/listItem.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const listItemSchema = new Schema({
   title: { type: String, required: true },
   notes: [{ type: Schema.Types.ObjectId, ref: 'Note' }],
+  isComplete: { type: Boolean, required: true, default: false },
+  isFavorite: { type: Boolean, required: true, default: false },
   listId: { type: Schema.Types.ObjectId, ref: 'List', required: true },
   dateCreated: { type: Date, default: Date.now },
 });
--- models/note.js
+++ models/note.js
@@ -5,6 +5,7 @@ const noteSchema = new Schema({
   text: { type: String, required: true },
   listItemId: { type: Schema.Types.ObjectId, ref: 'ListItem', required: true },
   dateCreated: { type: Date, default: Date.now },
+  isComplete: { type: Boolean, required: true, default: false },
 });
 
 const Note = mongoose.model('Note', noteSchema);
