0ef3325e7339de304a8f3cd396322601f46e6a6e
John Blackwell <john.blackwell@rtslabs.com>
2019-08-28 13:12:25 -0700

    update keys

--- keys.js
+++ keys.js
@@ -3,4 +3,9 @@ module.exports = {
     poolId: process.env.COGNITO_USER_POOL_ID,
     clientId: process.env.COGNITO_CLIENT_ID,
   },
+  cloudinary: {
+    cloud: process.env.CLOUDINARY_CLOUD_NAME,
+    key: process.env.CLOUDINARY_KEY,
+    secret: process.env.CLOUDINARY_SECRET,
+  },
 };
