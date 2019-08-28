609ffb700a6b2efd8dd6218e6bc5c47b98578e16
John Blackwell <john.blackwell@rtslabs.com>
2019-08-28 13:33:57 -0700

    add file ducks

--- client/src/store/ducks/index.js
+++ client/src/store/ducks/index.js
@@ -1,5 +1,6 @@
 import authReducer from './auth';
 import configReducer from './config';
+import fileReducer from './file';
 import userReducer from './user';
 const combineReducers = reducers => {
   return (state = {}, action) => {
@@ -13,11 +14,13 @@ const combineReducers = reducers => {
 const rootReducer = combineReducers({
   auth: authReducer,
   config: configReducer,
+  file: fileReducer,
   user: userReducer,
 });
 
 export { authInitialState as auth } from './auth';
 export { configInitialState as config } from './config';
+export { fileInitialState as file } from './file';
 export { userInitialState as user } from './user';
 
 export default rootReducer;
